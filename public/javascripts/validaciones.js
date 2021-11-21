e813b73341440587eec10153f7444ee52a4668d8
Rene PW <tallerista.pilares.rene@gmail.com>
2021-11-21 14:02:27 -0600

    Creando formulario con metodos POST y GET

--- public/javascripts/validaciones.js
+++ public/javascripts/validaciones.js
@@ -1,19 +1,23 @@
-function validarRadioButtom(){
-    const rbtMujer = document.querySelector('#mujer');
-    const rbtHombre = document.querySelector('#hombre');
-    if(validarChecked(rbtMujer)) {
-        alert(rbtMujer.value);
-        window.location.href = "/registro";
-        rbtMujer.checked = false;
-    }
-    else if(validarChecked(rbtHombre)){
-        alert(rbtHombre.value);
-        window.location.href = "/registro";
-        rbtHombre.checked = false;
-    }
-    else{alert("No seleccionado");}
+function mostrarFormulario(){
+    window.location.href = "/registro";
 }
 
-function validarChecked(radioBtn){
-    return radioBtn.checked == true;
-}
+// function validarRadioButtom(){
+//     const rbtMujer = document.querySelector('#mujer');
+//     const rbtHombre = document.querySelector('#hombre');
+//     if(validarChecked(rbtMujer)) {
+//         alert(rbtMujer.value);
+//         window.location.href = "/registro";
+//         rbtMujer.checked = false;
+//     }
+//     else if(validarChecked(rbtHombre)){
+//         alert(rbtHombre.value);
+//         window.location.href = "/registro";
+//         rbtHombre.checked = false;
+//     }
+//     else{alert("No seleccionado");}
+// }
+
+// function validarChecked(radioBtn){
+//     return radioBtn.checked == true;
+// }
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
-
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { titulo: 'Formularios' });
 });
 
-router.get('/registro', (req,res) =>{
+router.get('/registro', (req,res,next) =>{
   res.render('registro');
 });
+
+
+router.post('/registro', (req,res,next) =>{
+  res.render('registro', req.body)
+});
+
+
 
 module.exports = router;
