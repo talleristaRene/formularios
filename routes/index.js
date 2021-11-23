aad6d5a5234328d9405ad3b2321246649bfd7b03
Rene PW <tallerista.pilares.rene@gmail.com>
2021-11-23 12:45:44 -0600

    Se modifica el formulario

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+const archivosSistema = require('fs');
+const uuid = require('uuid').v4;
+
+const listaJson = archivosSistema.readFileSync('listaActividades.json', 'utf-8');
+let listaActividades = JSON.parse(listaJson);
+// const listaActividades = [];
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { titulo: 'Formularios' });
+  res.render('index', {  listaActividades});
 });
 
 router.get('/registro', (req,res,next) =>{
@@ -12,7 +19,27 @@ router.get('/registro', (req,res,next) =>{
 
 
 router.post('/registro', (req,res,next) =>{
-  res.render('registro', req.body)
+  // console.log(req.body.planchar);
+  const {platos,cocinar,barrer,planchar} = req.body;
+  if( !platos || !cocinar || !barrer || !planchar ){
+    res.status(400).send('Debes escribir todos los campos');
+    return;
+  }
+  let nuevaListaActividades = {
+      id: uuid(),
+      platos,
+      cocinar,
+      barrer,
+      planchar
+  }
+  listaActividades.push(nuevaListaActividades);
+
+
+  const listaJson = JSON.stringify(listaActividades);
+  archivosSistema.writeFileSync('listaActividades.json', listaJson, 'utf-8');
+
+  // res.send('recibido');
+  res.render('index', { listaActividades} ); 
 });
 
 
