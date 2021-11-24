1c7e2fc5a898649cf265e8e1be23004bf65811a1
Rene PW <tallerista.pilares.rene@gmail.com>
2021-11-24 10:25:06 -0600

    Se hicieron cambios en els servidor

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,9 @@ var router = express.Router();
 const archivosSistema = require('fs');
 const uuid = require('uuid').v4;
 
-const listaJson = archivosSistema.readFileSync('listaActividades.json', 'utf-8');
-let listaActividades = JSON.parse(listaJson);
-// const listaActividades = [];
+// const listaJson = archivosSistema.readFileSync('listaActividades.json', 'utf-8');
+// let listaActividades = JSON.parse(listaJson);
+const listaActividades = [];
 
 
 /* GET home page. */
@@ -13,35 +13,33 @@ router.get('/', function(req, res, next) {
   res.render('index', {  listaActividades});
 });
 
-router.get('/registro', (req,res,next) =>{
-  res.render('registro');
-});
+// router.get('/formularioGet', (req,res,next) =>{
+//   const qs =  req.body;
+//   console.log(qs);
+//   res.render('index',{listaActividades});
+  
+// });
 
+// router.post('/formularioPost', (req,res,next) =>{
+//   const {platos,cocinar,barrer,planchar, genero} = req.body;
+//   if( !platos || !cocinar || !barrer || !planchar  || !genero ){
+//     res.status(400).send('Debes escribir todos los campos');
+//     return;
+//   }
+//   let nuevaListaActividades = {
+//       id: uuid(),
+//       platos,
+//       cocinar,
+//       barrer,
+//       planchar,
+//       genero
+//   }
+//   listaActividades.push(nuevaListaActividades);
 
-router.post('/registro', (req,res,next) =>{
-  // console.log(req.body.planchar);
-  const {platos,cocinar,barrer,planchar} = req.body;
-  if( !platos || !cocinar || !barrer || !planchar ){
-    res.status(400).send('Debes escribir todos los campos');
-    return;
-  }
-  let nuevaListaActividades = {
-      id: uuid(),
-      platos,
-      cocinar,
-      barrer,
-      planchar
-  }
-  listaActividades.push(nuevaListaActividades);
+//   const listaJson = JSON.stringify(listaActividades);
+//   archivosSistema.writeFileSync('listaActividades.json', listaJson, 'utf-8');
 
-
-  const listaJson = JSON.stringify(listaActividades);
-  archivosSistema.writeFileSync('listaActividades.json', listaJson, 'utf-8');
-
-  // res.send('recibido');
-  res.render('index', { listaActividades} ); 
-});
-
-
+//   res.render('index', { listaActividades} ); 
+// });
 
 module.exports = router;
